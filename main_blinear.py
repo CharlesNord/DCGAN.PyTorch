798a2a78494e4f41b83fa036500cde49b20f97dd
TuXiaokang <931085856@qq.com>
2017-11-30 18:52:00 +0800

    update code

--- main_blinear.py
+++ main_blinear.py
@@ -13,7 +13,7 @@ from loader import get_loader, denorm
 from models.model import NetD, NetG
 
 parser = argparse.ArgumentParser(description='DCGAN')
-parser.add_argument('--dataset', type=str, default='../data/celeba/img_align_celeba')
+parser.add_argument('--dataset', type=str, default='../data/celeba')
 parser.add_argument('--image_size', type=int, default=128)
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--num_workers', type=int, default=8)
